6a60be2c141bd29aeae497db1c6e5ce1a019b553
Sean Gilday <spgilday19@gmail.com>
2024-04-25 22:21:27 -0400

    Rough draft of NavBar

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import NavBar from "./NavBar.jsx";
 import Homepage from "./Homepage.jsx";
 import API from "./API.jsx";
 import AboutUs from "./AboutUs.jsx";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 
 //Global styling for the website
 const GlobalStyle = createGlobalStyle`
@@ -15,11 +16,13 @@ const GlobalStyle = createGlobalStyle`
 export default function App() {
     return (
         <>
-            <GlobalStyle />
-            <NavBar/>
-            <Homepage/>
-            <API/>
-            <AboutUs/>
+            <BrowserRouter>
+                <NavBar/>
+                <Routes>
+                    <Route path='/' element={<Homepage/>} />
+                    <Route path='/about-us' element={<AboutUs/>}/>
+                </Routes>
+            </BrowserRouter>
         </>
     );
 }
--- src/Homepage.jsx
+++ src/Homepage.jsx
@@ -1,7 +1,12 @@
 // Teresa's component
 import React from 'react';
+import API from './API.jsx'
 export default function Homepage() {
     return (
-        <h1>Homepage</h1>
+        <>
+            <h1>Homepage</h1>
+            <API/>
+        </>
+
     )
 }
